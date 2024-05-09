13cb67858fe890b04c7bd1a9debe6cd35b859835
HASSAN ZAMIN <162346010+bsc-44-21@users.noreply.github.com>
2024-05-09 03:08:55 -0700

    Creating folder component on market as a feature that holds my pages that user will be routing from one after the other to look for the product its location and price

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="title">MY FARM MALAWI</div>;
 }
 
 export default App;
